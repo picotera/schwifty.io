26740c74dac8d36117e9ad6c4dd72aba75f0c81a
Amit Netanel <netanel.amit@gmail.com>
2017-01-21 12:29:50 +0200

    added the canvas testing folder

---
+++ canvas/model.js
@@ -0,0 +1,17 @@
+var global_music = {
+  [{
+    "id": 1,
+    "rgb_correct": [20,29,205],
+    "rgb_error": [210,16,16],
+    "data": [
+      {"weight": 1, "type": 0},
+      {"weight": 1, "type": 1},
+      {"weight": 1, "type": 0},
+      {"weight": 1, "type": 1}
+    ]
+  ,
+
+
+
+  ]
+}
